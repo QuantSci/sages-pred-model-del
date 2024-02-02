7e43979a33de4d6ae3f6f8097788e0e4c2f7ea92
dougtommet <doug_tommet@brown.edu>
2024-02-02 09:07:10 -0500

    Changed the lab variables to continuous

--- R/060-implement_rms.R
+++ R/060-implement_rms.R
@@ -4,7 +4,7 @@ sages_combined <-   readRDS(file=fs::path(r_objects_folder, "020_sages_combined.
 variable_df <- readRDS(file=fs::path(r_objects_folder, "021_variable_df.rds"))
 
 covariates_to_use <- variable_df %>%
-  filter(covariates_to_use_2) %>%
+  filter(covariates_to_use_3) %>%
   pull(covariates_all)
 
 sages_combined <- sages_combined %>%
